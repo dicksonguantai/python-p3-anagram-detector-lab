40b26887de459b58477729bdfc8fe147f90e22f3
dicksonmutwiriguantai <dickson.mutwiri@student.moringaschool.com>
2023-12-06 21:31:13 +0300

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+class Anagram:
+
+    def __init__(self,word):
+        self.word = word
+
+
+    def match(self,list):
+        checkable_word = sorted(self.word)
+
+        found_anagram = [word for word in list if sorted(word)== checkable_word]
+
+        return found_anagram
